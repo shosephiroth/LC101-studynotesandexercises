1a9f3af1e938127fb1440202de6cf6e15d158de1
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-06-09 19:44:48 -0400

    Create class1QuizQuestion9.js
    broken down to better understand

---
+++ class1QuizQuestion9.js
@@ -0,0 +1,12 @@
+// So I'm not sure how this solution is coming about
+
+// 16 - 2 * 5 / 3 + 1
+
+// answer is 13.6666.., but how?
+
+let num1 = 2 * 5
+num1 = num1 / 3
+num1 = 16 - num1
+num1 = num1 + 1
+
+console.log(num1);
