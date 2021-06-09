df63964e60643e20b18d8261eaafa7bb7958c888
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-06-09 17:36:12 -0400

    Update InputExamples01.js
    updated code as requested by exercise

--- InputExamples01.js
+++ InputExamples01.js
@@ -1,5 +1,17 @@
+/* Try It
+Update your code to request a user's first and last name, then print an output that looks like:
+
+First name: Elite
+Last name: Coder
+Last, First: Coder, Elite
+*/ 
+
 const input = require(`readline-sync`);
 
-let name = input.question("Enter your name: ");
+let firstName = input.question("Enter your first name: ");
+let lastName = input.question("Enter your last name: ");
 
-console.log(name);
+
+console.log("First name: " + firstName);
+console.log("Last name: " + lastName);
+console.log("Last, First: " + firstName);
