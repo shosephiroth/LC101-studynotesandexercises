1b5ffbb3c0679ffa964aebbea7ffbd81e86cce07
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-06-13 16:18:23 -0400

    Update ConditionalsExercises02.js
    almost completed exercise just have to guess/predict for exercise 4/D

--- ConditionalsExercises02.js
+++ ConditionalsExercises02.js
@@ -9,12 +9,30 @@ let shuttleSpeed = 15000;
 
 // a) If crewStatus is true, print "Crew Ready" else print "Crew Not Ready".
 
+if (crewStatus = spaceSuitsOn && shuttleCabinReady) {
+   console.log("Crew ready");
+} else {
+   console.log(" Crew not ready");
+}
 
 // b) If computerStatusCode is 200, print "Please stand by. Computer is rebooting." Else if computerStatusCode is 400, print "Success! Computer online." Else print "ALERT: Computer offline!"
+
+if (computerStatusCode === 200) {
+   console.log("Please stand by. Computer is rebooting.") 
+} else if (computerStatusCode === 400) {
+   console.log("ALERT: Computer is offline!");
+}
 
 
 // c) If shuttleSpeed is > 17,500, print "ALERT: Escape velocity reached!" Else if shuttleSpeed is < 8000, print "ALERT: Cannot maintain orbit!" Else print "Stable speed".
 
+if (shuttleSpeed > 17,500) {
+   console.log("ALERT: Escape velocity reached!");
+} else if (shuttleSpeed < 8000) {
+   console.log("ALERT: Cannot maintain orbit!");
+} else {
+   console.log("Stable speed");
+}
 
 // 4) PREDICT: Do the code blocks shown in the 'predict.txt' file produce the same result?
 
