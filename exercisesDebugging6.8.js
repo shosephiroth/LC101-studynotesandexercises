04cef13da4f40e719ed23574ca0c9cbab729535a
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-06-13 19:49:04 -0400

    Create exercisesDebugging6.8.js
    initial commit

---
+++ exercisesDebugging6.8.js
@@ -0,0 +1,15 @@
+// 6.8. Exercises: Debugging
+
+//Run this code first and examine the error message.
+//Fix the syntax error then run the code again to check your work.
+
+let launchReady = false;
+let fuelLevel = 17000;
+
+if (fuelLevel >= 20000 {
+   console.log('Fuel level cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Insufficient fuel!');
+   launchReady = false;
+}
