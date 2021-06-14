b6b3887233dd489430b0a26a407537ab7168b3f7
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-06-13 21:05:52 -0400

    Update exercisesDebugging6.8.js
    added new variable so launchready wasn't used twice, thus turning the boolean from false to true when in fact it's not due to previous code block's conditions.

--- exercisesDebugging6.8.js
+++ exercisesDebugging6.8.js
@@ -8,6 +8,7 @@ let launchReady = false;
 let fuelLevel = 17000;
 let crewStatus = true;
 let computerStatus = 'green';
+let launchStatus = false;
 
 if (fuelLevel >= 20000) {
    console.log('Fuel level cleared.');
@@ -21,7 +22,7 @@ console.log("launchReady = ", launchReady);
 
 if (crewStatus && computerStatus === 'green'){
    console.log('Crew & computer cleared.');
-   launchReady = true;
+   launchStatus = true;
 } else {
    console.log('WARNING: Crew or computer not ready!');
    launchReady = false;
