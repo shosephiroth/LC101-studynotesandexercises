31e0d1689f423c94afa11b0ef9958aa7831a70e2
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-06-15 20:31:30 -0400

    Update chapter7StudyNotes.js
    more notes added

--- chapter7StudyNotes.js
+++ chapter7StudyNotes.js
@@ -10,3 +10,21 @@ let outputStr = "Javascript was not created by someone with the initials " + fir
 
 console.log(outputStr);
 
+// What does an expression using bracket notation evaluate to when the index is invalid?
+
+// (the index does not correspond to a character in the string)?
+
+// Answer: Undefined
+
+// 7.2.1 Check your understanding
+
+// if phrase = 'Code for fun', then phrase[2] evaluates to:
+
+// Answer: d 
+
+
+// Which of the following returns true given myStr = 'Index'?
+
+//Answer:
+// myStr[4] = 'x';
+// myStr[0] = 'I';
