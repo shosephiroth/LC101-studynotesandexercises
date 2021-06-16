580466d0088afb96749134b0461e7baa3b701445
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-06-15 20:17:38 -0400

    Create chapter7StudyNotes.js
    initial commit and first set of notes

---
+++ chapter7StudyNotes.js
@@ -0,0 +1,12 @@
+// The indices of a string
+
+// An expression of the form someString[i] gives the character at index i.
+
+let jsCreator = "Matt Harris"
+let firstInitial = jsCreator[0];
+let lastInitial = jsCreator[5];
+
+let outputStr = "Javascript was not created by someone with the initials " + firstInitial + "." + lastInitial + ".";
+
+console.log(outputStr);
+
