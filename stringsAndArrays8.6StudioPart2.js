5c317fca2f9b610137949c50a2dcf135d15eda32
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-06-18 00:22:13 -0400

    Create stringsAndArrays8.6StudioPart2.js
    I honestly don't even know how I got here

---
+++ stringsAndArrays8.6StudioPart2.js
@@ -0,0 +1,60 @@
+let protoArray1 = "3,6,9,12";
+let protoArray2 = "A;C;M;E";
+let protoArray3 = "space delimited string";
+let protoArray4 = "Comma-spaces, might, require, typing, caution";
+
+strings = [protoArray1, protoArray2, protoArray3, protoArray4];
+
+//a) Use the 'includes' method to check to see if the words in each string are separated by commas (,), semicolons (;) or just spaces.
+
+//console.log(strings.includes(";"));
+
+let testStr0 = strings;
+
+if (testStr0.includes("," || ";" || " ")){
+   console.log("One of them is here");
+    else if {
+      testStr0.includes(",");
+    }
+} else {
+   console.log("Nope. False");
+}
+
+let testStr1 = strings[0];
+
+if (testStr1.includes("," || ";" || " ")){
+   console.log("One of them is here");
+} else {
+   console.log("Nope. False");
+}
+
+let testStr2 = strings[1];
+
+if (testStr2.includes("," || ";" || " ")){
+   console.log("One of them is here");
+} else {
+   console.log("Nope. False");
+}
+
+let testStr3 = strings[2];
+
+if (testStr3.includes("," || ";" || " ")){
+   console.log("One of them is here");
+} else {
+   console.log("Nope. False");
+}
+
+
+//b) If the string uses commas to separate the words, split it into an array, reverse the entries, and then join the array into a new comma separated string.
+
+if testStr0.includes()
+
+//c) If the string uses semicolons to separate the words, split it into an array, alphabetize the entries, and then join the array into a new comma separated string.
+
+
+
+//d) If the string uses spaces to separate the words, split it into an array, reverse alphabetize the entries, and then join the array into a new space separated string.
+
+
+
+//e) If the string uses ‘comma spaces’ to separate the list, modify your code to produce the same result as part “b”, making sure that the extra spaces are NOT part of the final string.
