77e1e4e1481c90aa6295897f14128beb930927f2
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-06-19 17:22:19 -0400

    Update stringExercises05.js
    completed part 1 of 3

--- stringExercises05.js
+++ stringExercises05.js
@@ -8,10 +8,15 @@ console.log(language.slice(0,1) + (language.slice(4,5)));
 
 //b) Without using .slice(), use method chaining to accomplish the same thing.
 
+console.log(language[0] + language[4]);
+
 
 
 //c) Use bracket notation and a template literal to print "The abbreviation for 'JavaScript' is 'JS'."
 
+console.log(`The abbreviation for '${language}' is ` + language[0] + language[4]);
 
 
 //d) Just for fun, try chaining 3 or more methods together, and then print the result.
+
+console.log(`Chaining 3 methods together using ${language}` + " also known as " + language.slice(0,1) +language[4]);
