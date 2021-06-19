904ddb6031da15a2cbda0ca846b82b4373e02149
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-06-19 15:30:57 -0400

    Create candidateTestingPart2.js
    initial commit

---
+++ candidateTestingPart2.js
@@ -0,0 +1,63 @@
+const input = require('readline-sync');
+
+// TODO 2: modify your quiz app to ask 5 questions //
+
+// TODO 1.1a: Define candidateName // 
+let candidateName;
+// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
+let question = "Who was the first woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer;
+let questions;
+let correctAnswers;
+let candidateAnswers;
+
+
+function askForName() {
+  // TODO 1.1b: Ask for candidate's name //
+  candidateName = input.question("What is your name? ");
+
+}
+
+function askQuestion() {
+  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+candidateAnswer = input.question(question);
+}
+
+function gradeQuiz(candidateAnswers) {
+
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+if (candidateAnswer === correctAnswer) {
+   console.log("You are correct! *DJ Horn plays*");
+} else {
+   console.log("You are incorrect. Womp womp!");    
+}
+
+  let grade;
+  
+
+  return grade;
+}
+
+function runProgram() {
+  askForName();
+  // TODO 1.1c: Ask for candidate's name //
+  console.log("Hello, " + candidateName);
+  
+  askQuestion();
+  gradeQuiz(this.candidateAnswers);
+}
+
+// Don't write any code below this line //
+// And don't change these or your program will not run as expected //
+module.exports = {
+  candidateName: candidateName,
+  question: question,
+  correctAnswer: correctAnswer,
+  candidateAnswer: candidateAnswer,
+  questions: questions,
+  correctAnswers: correctAnswers,
+  candidateAnswers: candidateAnswers,
+  gradeQuiz: gradeQuiz,
+  runProgram: runProgram
+};
