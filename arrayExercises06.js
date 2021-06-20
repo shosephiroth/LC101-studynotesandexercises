1c3350b0b3b211d8feacbde5c118fffaaf85055b
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-06-20 00:29:41 -0400

    Update arrayExercises06.js
    I did it, exercise complete

--- arrayExercises06.js
+++ arrayExercises06.js
@@ -12,12 +12,36 @@ table = [];
 table.push(element1, element2, element3);
 console.log(table);
 
+console.log();
+
 //c) Use bracket notation to examine the difference between printing 'table' with one index vs. two indices (table[][]).
 
+console.log(table[0]);
+console.log(table[0][2]);
 
+console.log();
 
-//d) Using bracket notation and the table array, print the mass of element1, the name for element 2 and the symbol for element26.
+console.log(table[1]);
+console.log(table[1][0]);
+
+console.log();
+
+//d) Using bracket notation and the table array, print the mass of element1, the name for element 2 and the symbol for element26. // element 3?
+
+console.log(table[0][2] + " " + table[1][0] + " " + table[2][1]);
+
+console.log();
 
 
 
 //e) 'table' is an example of a 2-dimensional array. The first “level” contains the element arrays, and the second level holds the name/symbol/mass values. Experiment! Create a 3-dimensional array and print out one entry from each level in the array.
+
+let table2 = [];
+
+table2 = table;
+
+let table3 = table2;
+
+let table3D =[table, table2, table3];
+console.log(table3D);
+
