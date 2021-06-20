ed07e67548c50f46ab70fdec95a9523137b9e8db
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-06-19 23:47:33 -0400

    Update arrayExercises06.js
    Not done yet but Repl is freezing. Gonna restart it.

--- arrayExercises06.js
+++ arrayExercises06.js
@@ -1,12 +1,16 @@
 //Arrays can hold different data types, even other arrays! A multi-dimensional array is one with entries that are themselves arrays.
 
 //a) Define and initialize the arrays specified in the exercise to hold the name, chemical symbol and mass for different elements.
+element1 = ['hydrogen', 'H', 1.000];
+element2 = ['helium', 'He', 4.003];
+element3 = ['iron', 'Fe', 55.85];
 
 
 
 //b) Define the array 'table', and use 'push' to add each of the element arrays to it. Print 'table' to see its structure.
-
-
+table = [];
+table.push(element1, element2, element3);
+console.log(table);
 
 //c) Use bracket notation to examine the difference between printing 'table' with one index vs. two indices (table[][]).
 
