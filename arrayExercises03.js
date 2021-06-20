b8d5d8cbf71ea1e357e415a4a10d6d6689752365
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-06-19 22:52:54 -0400

    Update arrayExercises03.js
    completed exercise

--- arrayExercises03.js
+++ arrayExercises03.js
@@ -4,10 +4,17 @@ let cargoHold = [1138, 'space suits', 'parrot', 'instruction manual', 'meal pack
 
 //a) Insert the string 'keys' at index 3 without replacing any other entries.
 
+cargoHold.splice(3, 0, 'keys');
+console.log(cargoHold);
 
 
 //b) Remove ‘instruction manual’ from the array. (Hint: indexOf is helpful to avoid manually counting an index).
 
-
+cargoHold.indexOf('instruction manual');
+cargoHold.splice(4, 1);
+console.log(cargoHold);
 
 //c) Replace the elements at indexes 2 - 4 with the items ‘cat’, ‘fob’, and ‘string cheese’.
+
+cargoHold.splice(2, 3, 'cat', 'fob', 'string cheese');
+console.log(cargoHold);
