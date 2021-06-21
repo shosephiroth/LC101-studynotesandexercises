0fd0fdb298984c6cb58dc6e8cde9fd7059d46317
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-06-21 17:55:58 -0400

    Update candidateTestingPart2.js
    DUH put the questions as short named variables like q1, q2 and put those in the array instead of raw long strings.

--- candidateTestingPart2.js
+++ candidateTestingPart2.js
@@ -8,9 +8,15 @@ let candidateName;
 let question = "Who was the first woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer;
-let questions = ["Who was the first American woman in space?", "True or false: 5 kilometer == 5000 meters?", "(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
-let correctAnswers =["Sally Ride", "true", "40", "Trajectory", "3"];
+let questions = [q1, q2, q3, q4, q5];
+let correctAnswers =[];
 let candidateAnswers;
+// put questions in variables then put variables in array
+let q1 = "Who was the first American woman in space?";
+let q2 = "True or false: 5 kilometer == 5000 meters?";
+let q3 = "(5 + 3)/2 * 10 = ?";
+let q4 = "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?";
+let q5 = "What is the minimum crew size for the ISS?";
 
 
 function askForName() {
