2c565a455255712e8375bbda5592d08b5ada2fab
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-06-23 22:04:44 -0400

    Update chapter9Notes.js
    finished chapter 9

--- chapter9Notes.js
+++ chapter9Notes.js
@@ -131,3 +131,20 @@ while (num <= 0) {
   num = input.question('Invalid Input. Please enter a positive number:');
   num = Number(num);
 }
+
+let numbers = [/* some numbers */] ;
+let searchVal = 41;
+i = 0;
+
+while (i <numbers.length) {
+   if (numbers[i] === searchVal) {
+      break;
+   }
+   i++;
+}
+
+if (i < numbers.length) {
+   console.log("The value", searchVal, "was located at index", i);
+} else {
+   console.log("The value", searchVal, "is not in the array.");
+}
