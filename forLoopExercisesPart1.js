8f905f936001f62bebb81a6c4d59ae46fac522c5
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-06-23 22:30:01 -0400

    Update forLoopExercisesPart1.js
    update first part a/b/c complete but part d is not printing. Tried changing the variables and everything. Will come back to it later

--- forLoopExercisesPart1.js
+++ forLoopExercisesPart1.js
@@ -3,9 +3,30 @@
     b. Print only the ODD values from 3 - 29, one number per line.
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
-
-
-
+//a
+for (let i = 0; i < 21; i++) {
+   console.log(i);
+}
+console.log();
+//b 
+for (let i = 0; i < 30; i = i + 3) {
+   console.log(i);
+}
+console.log();
+//c 
+for (let i = 12; i > -15; i = i - 2) {
+   console.log(i);
+}
+console.log();
+//d 
+for (let i = 50; i > 20; i--) {
+  if (i % 3 === 0) {
+   console.log(i);  
+  } else {
+     break;
+  }
+   
+}
 
 /*Exercise #2: 
 Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
