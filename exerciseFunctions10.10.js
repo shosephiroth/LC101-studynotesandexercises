f0fbf63277324d4604453bbd9d84f892be40e11f
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-06-24 17:12:26 -0400

    Update exerciseFunctions10.10.js
    I wasn't even close..

--- exerciseFunctions10.10.js
+++ exerciseFunctions10.10.js
@@ -1,21 +1,13 @@
 // Write a function makeLine(size) that returns a line with exactly size hashes.
 
-// attempt 1
-const input = require('readline-sync');
+// The actual solution
 
 function makeLine(size) {
- let i = input.question('Pick a number, 1-10');
- i = (i * size);
- return i;
+   let line = '';
+   for (let i = 0; i < size; i++) {
+      line += '#';
+   }
+   return line;
 }
 
-makeLine(3); 
-
-// attempt 2
-
-function makeLine2(size) {
-   let i = '#';
-   return (size * i);
-}
-
-makeLine2(5);
+makeLine(5);
