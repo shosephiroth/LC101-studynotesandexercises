65329da5bfc2540171587e9241289b10cd17a819
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-06-24 20:35:27 -0400

    Update studioFunctions.js
    got this in class studio with group and Sierra's help

--- studioFunctions.js
+++ studioFunctions.js
@@ -9,25 +9,10 @@
 
 
 
-
 //2) The 'split' method does not work on numbers, but we want the function to return a number with all the digits reversed (e.g. 1234 converts to 4321 and NOT the string "4321")
 //a) Add an if statement to your reverseCharacters function to check the typeof the parameter.
 //b - d) If typeof is ‘string’, return the reversed string as before. If typeof is ‘number’, convert the parameter to a string, reverse the characters, then convert it back into a number. Return the reversed number.
 //e) Be sure to print the result returned by the function to verify that your code works for both strings and numbers. Do this before moving on to the next exercise.
-
-function reverseCharacters(str) {
-  if typeof(str) == String(str) {
-   let lettersArray = str.split('');
-   let reversedLettersArray = lettersArray.reverse();
-   return reversedLettersArray.join('');
-  } else if {
-     typeof(str)!== Number(str);
-      str = Number(str);
-  }
-     
-}
-// test 
-reverseCharacters('Colonel angus');
 
 //3) Create a new function with one parameter, which is the array we want to change. The function should:
 //a) Define and initialize an empty array.
@@ -36,9 +21,31 @@ reverseCharacters('Colonel angus');
 //d) Add the reversed string (or number) to the array defined in part ‘a’.
 //e) Return the final, reversed array.
 //f) Be sure to print the results from each test case in order to verify your code.
-
-
-
 let arrayTest1 = ['apple', 'potato', 'Capitalized Words'];
 let arrayTest2 = [123, 8897, 42, 1168, 8675309];
 let arrayTest3 = ['hello', 'world', 123, 'orange'];
+
+function reverseCharacters(element) {
+  if (typeof element === 'number') {
+    element = Number(String(element).split("").reverse().join(""));
+  } else {
+    element = element.split("").reverse().join("");
+  }
+  
+  return element;
+}
+
+
+
+let reversedString = 'Hello from struggle city';
+console.log(reverseCharacters(reversedString));
+
+function newFunction(arrayFunc) {
+   let array2 = [];
+   for (let i = 0; i < arrayFunc.length; i++) {
+     array2.push(reverseCharacters(arrayFunc[i]));
+
+   }
+  return array2;
+}
+console.log(newFunction(arrayTest2))
