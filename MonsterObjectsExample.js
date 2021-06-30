14551e7ad88c587f601b43a66dfb4a4a93d16e34
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-06-30 18:49:44 -0400

    Create MonsterObjectsExample.js
    initial commit. Practice from chapter 12: Objects and the Math Object

---
+++ MonsterObjectsExample.js
@@ -0,0 +1,25 @@
+let dragon = {
+  species: 'lizard',
+  hp: 100,
+  atk: 25,
+  def: 25,
+  speed: 5
+}
+
+let dog = {
+  species: 'mammal',
+  hp: 15,
+  atk: 5,
+  def: 5,
+  speed: 15
+}
+
+let cat = {
+  species: 'mammal',
+  hp: 10,
+  atk: 5,
+  def: 7,
+  speed: 20
+}
+
+console.log(cat);
