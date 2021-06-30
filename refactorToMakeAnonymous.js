d4fb7dacbdab870e01cfac861d028c8ce25f0b6c
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-06-29 23:33:04 -0400

    Update and rename refactorToMakeAnonymous to refactorToMakeAnonymous.js
    completed objective: Convert the following named function to an anonymous function that is stored in a variable.

--- refactorToMakeAnonymous.js
+++ refactorToMakeAnonymous.js
@@ -1,7 +1,7 @@
-function reverse(str) {
+reverseStr = function (str) {
   let lettersArray = str.split('');
   let reversedLettersArray = lettersArray.reverse();
   return reversedLettersArray.join('');
 }
 
-console.log(reverse("LaunchCode"));
+console.log(reverseStr("LaunchCode"));
