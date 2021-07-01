69ff3e62b9e2efa2926d1ff427414ae393e6b7f8
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-07-01 18:32:13 -0400

    Create quiz2StringArray.js
    initial commit; quiz!! Does not count against grade, just checking for understanding

---
+++ quiz2StringArray.js
@@ -0,0 +1,15 @@
+const quiz = require('./quiz.js');
+
+
+// let Jasmine = require('jasmine');
+// let jasmine = new Jasmine();
+
+// jasmine.loadConfig({
+//   spec_dir: 'spec',
+//   spec_files: [
+//     "**/*[sS]pec.js"
+//   ],
+// });
+
+// jasmine.execute();
+
