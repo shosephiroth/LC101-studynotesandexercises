77f2a540074ad249f91265854bc94a387d3d3a4c
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-07-01 18:33:53 -0400

    Update quiz2StringArray.js
    index.js committed by accident not actual quiz, quiz.js. Real initial commit

--- quiz2StringArray.js
+++ quiz2StringArray.js
@@ -1,15 +1,40 @@
-const quiz = require('./quiz.js');
+// DO NOT HARDCODE ANY OF THE ANSWER
+
+// STRINGs
+const phrase = "The cow jumped over the moon";
+
+// get the last word of the phrase 
+let lastWord;
+
+// get the first word in all upper case
+let firstWord;
+
+// get the last index of 'o'
+// use a method
+let lastO;
 
 
-// let Jasmine = require('jasmine');
-// let jasmine = new Jasmine();
+// ARRAYs
+// initialize prime to a empty array
+let primes;
 
-// jasmine.loadConfig({
-//   spec_dir: 'spec',
-//   spec_files: [
-//     "**/*[sS]pec.js"
-//   ],
-// });
+// add the number 1 to the array
 
-// jasmine.execute();
+// add the number 3 to the array
 
+// add the number 5 to the array
+
+// get the array length
+let arrayLength;
+
+// LOOPS
+
+// removes the letter 'm' from the phrase
+// do not use 'replaceAll'
+// HINT: Use a for loop to look at each character
+let noM;
+
+// similar to acronymn, but instead of getting the first character, get the last character of each word in the phrase 'The cow jumped over the moon'
+// For example if the word is 'Launch Code', the endingChars is 'he'. 'h' from Launch and 'e' from Code. 
+// The expected answer for this is 'ewdren'
+let endingChars;
