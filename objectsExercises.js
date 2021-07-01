ab272ee4daa4731b84931659b08278c17a3c6566
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-07-01 17:52:35 -0400

    Update objectsExercises.js
    this is what I got done so far, the code works but doesn't do much yet.

--- objectsExercises.js
+++ objectsExercises.js
@@ -2,15 +2,65 @@ let superChimpOne = {
     name: "Chad",
     species: "Chimpanzee",
     mass: 9,
-    age: 6
+    age: 6,
+    astronautID: 1,
+    move: function() {
+      move = Math.floor(Math.random() * 11);
+      return move;
+    }
 };
 
 let salamander = {
     name: "Lacey",
     species: "Axolotl Salamander",
     mass: 0.1,
-    age: 5
+    age: 5,
+    astronautID: 2,
+    move: function() {
+      move = Math.floor(Math.random() * 11);
+      return move;
+    }
 };
+
+let superChimpTwo = {
+    name:"Brad",
+    species: "Chimpanzee",
+    mass: 11,
+    age: 6,
+    astronautID: 3,
+    move: function() {
+      move = Math.floor(Math.random() * 11);
+      return move;
+    }
+};
+
+let busyBeagle = {
+    name: "Leroy",
+    species: "Beagle",
+    mass: 14,
+    age: 5,
+    astronautID: 4,
+    move: function() {
+      move = Math.floor(Math.random() * 11);
+      return move;
+    }
+};
+
+let terrificTardigrade = {
+    name: "Almina",
+    species: "Tardigrade",
+    mass: 0.0000000001,
+    age: 1,
+    astronautID: 5,
+    move: function() {
+      move = Math.floor(Math.random() * 11);
+      return move;
+    }
+};
+
+console.log(superChimpOne.move());
+
+astronautsCrewArray = [];
 
 
 // After you have created the other object literals, add the astronautID property to each one.
