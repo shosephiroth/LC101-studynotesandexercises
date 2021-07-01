1d65babe032219f54a99d62794674d384da57379
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-07-01 18:34:29 -0400

    Update quiz2StringArray.js
    did not finish quiz

--- quiz2StringArray.js
+++ quiz2StringArray.js
@@ -4,28 +4,44 @@
 const phrase = "The cow jumped over the moon";
 
 // get the last word of the phrase 
-let lastWord;
+let lastWord = phrase.split(' ');
+//console.log(lastWord);
+console.log(lastWord[lastWord.length-1]);
 
 // get the first word in all upper case
-let firstWord;
+let firstWord = phrase.split(' ');
+//console.log(firstWord);
+console.log(firstWord[0]);
 
 // get the last index of 'o'
-// use a method
 let lastO;
+// for (let i = 0; i < lastO.length - 1);
+//  last0.includes('o');
 
 
 // ARRAYs
 // initialize prime to a empty array
-let primes;
+let primes =[];
 
 // add the number 1 to the array
 
+primes.push(1);
+console.log(primes);
+
 // add the number 3 to the array
+
+primes.push(3);
+console.log(primes);
 
 // add the number 5 to the array
 
+primes.push(5);
+console.log(primes);
+
 // get the array length
 let arrayLength;
+arrayLength = primes.length 
+console.log(arrayLength);
 
 // LOOPS
 
@@ -33,8 +49,47 @@ let arrayLength;
 // do not use 'replaceAll'
 // HINT: Use a for loop to look at each character
 let noM;
+for (let i = 0; i < lastWord.length; i++) {
+    lastWord.split('M');
+    
+}
+console.log(noM);
 
 // similar to acronymn, but instead of getting the first character, get the last character of each word in the phrase 'The cow jumped over the moon'
 // For example if the word is 'Launch Code', the endingChars is 'he'. 'h' from Launch and 'e' from Code. 
 // The expected answer for this is 'ewdren'
 let endingChars;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// YOU'RE DONE!!!!
+
+// DO NOT MODIFY
+module.exports = {
+  lastWord: lastWord,
+  firstWord: firstWord,
+  lastO: lastO,
+  noM: noM,
+  endingChars: endingChars,
+  primes: primes,
+  arrayLength: arrayLength
+}
