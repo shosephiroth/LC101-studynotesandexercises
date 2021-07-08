b8236dc7f3d63cdd896072ec06ebf7c56bdfe097
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-07-08 18:30:23 -0400

    Create quiz3FunctionLoops.js
    initial commit

---
+++ quiz3FunctionLoops.js
@@ -0,0 +1,33 @@
+// Question #1
+// create a function named fullName that takes in two params, firstName and lastName.
+// return a string with the name combined
+// Example:
+// firstName = "Grace"
+// lastName = "Hopper"
+// The function should return "Grace Hopper"
+
+
+
+// Question #2
+// Similar to acronym, this functions will return the last character of each word in upper case
+// For example, if the title is "Launch Code", the result should be "HE"
+function backcronym(title) {
+
+
+}
+
+// Question #3
+// Given an array of numbers, find the smallest number in the array
+// you can use a library function or write your own
+function minValue(numbers) {
+
+}
+
+
+// Bonus Question
+// Given an array of numbers, find the number that appears the most
+// For example, given [4, 1, 6, 2, 1, 2, 5, 2]
+// the function should return 2, because 2 appears 3 times, more than the other numbers
+function mostCommon(numbers) {
+
+}
