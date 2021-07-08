ae1aa5875f9018d8be66704cc53de3f3b173c26e
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-07-08 18:30:46 -0400

    Update quiz3FunctionLoops.js
    updated

--- quiz3FunctionLoops.js
+++ quiz3FunctionLoops.js
@@ -6,22 +6,48 @@
 // lastName = "Hopper"
 // The function should return "Grace Hopper"
 
+function fullName (firstName, lastName) {
+  firstAndLastName = firstName + ' ' + lastName;
+  console.log(firstAndLastName);
+}
+//test
+fullName('Matt', 'Harris');
 
 
 // Question #2
 // Similar to acronym, this functions will return the last character of each word in upper case
 // For example, if the title is "Launch Code", the result should be "HE"
+
+title = 'Launch Code';
+
 function backcronym(title) {
+  title = title + ' ';
+  let title2 =[];
+  for (let i=1; i < title.length; i++) {
+     if (title[i] === ' ') {
+        title2.push((title[i - 1] + ' '));
+        console.log(title2);
+     }
+  }
 
 
 }
+
+backcronym(title);
 
 // Question #3
 // Given an array of numbers, find the smallest number in the array
 // you can use a library function or write your own
 function minValue(numbers) {
+  let smallestNum = numbers.sort((a, b) => a - b);
+  console.log(smallestNum[0]);
 
 }
+
+array1 = [4, 1, 6, 2, 1, 2, 5, 2];
+
+minValue(array1);
+
 
 
 // Bonus Question
