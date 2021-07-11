aa9c98f36081f6f1a82810824ef0d7a0321f3fea
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-07-11 16:22:06 -0400

    Update assignment2ScrabbleScorer.js
    still not working lol

--- assignment2ScrabbleScorer.js
+++ assignment2ScrabbleScorer.js
@@ -64,7 +64,7 @@ let simpleScore = function (word) {
 
 let vowelBonusScore = function (word) {
  for (let i = 0; i < word.length; i++) {
-    if (word.includes('a', 'e', 'i', 'o', 'u')) {
+    if (word.includes(oldPointStructure[i])) {
       //test
       console.log(word[i]);
       //return i;
