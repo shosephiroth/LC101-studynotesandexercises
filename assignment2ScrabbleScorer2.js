628e180f66f119f811893207de19627da7996370
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-07-14 16:24:10 -0400

    Update assignment2ScrabbleScorer2.js
    functional, working on transform function now. trying some things that are not currently visible in this version, thanks for the save undo button

--- assignment2ScrabbleScorer2.js
+++ assignment2ScrabbleScorer2.js
@@ -190,9 +190,33 @@ function scorerPrompt() {
 // these console logs above currently do not work and point to
 // the original array being a dictionary not an array possibly?
 
-function transform() {};
+// Part C 
 
-let newPointStructure;
+/* Transform the Object
+1. Write the rest of the transform() function. It will need to take an object as a parameter - specifically the oldPointStructure object. Calling transform(oldPointStructure) will return an object with lowercase letters as keys. The value for each key will be the points assigned to that letter. */
+
+/*Recall that for...in loops iterate over the keys within an object.
+
+If you need a reminder of how to assign new key/value pairs, review the relevant section in the Objects and Math chapter.
+
+To access the letter arrays within oldPointStructure, use bracket notation (oldPointStructure['key']).
+
+To access a particular element within a letter array, add a second set of brackets (oldPointStructure['key'][index]), or assign the array to a variable and use variableName[index]. */
+
+function transform(object) {
+  for (item in object) {
+    //test
+     console.log(`${item}  ${object[item]}`);
+  }
+
+}
+
+
+// 2. Locate the newPointStructure object in the starter code and set it equal to transform(oldPointStructure). 
+
+let newPointStructure = transform(oldPointStructure);
+
+console.log(newPointStructure);
 
 function runProgram() {
    initialPrompt();
