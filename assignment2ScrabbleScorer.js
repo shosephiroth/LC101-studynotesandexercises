d6883a44262f476d2a908369ef24d8e2ed49491f
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-07-14 17:36:18 -0400

    Update assignment2ScrabbleScorer.js
    More interesting logs/returns

--- assignment2ScrabbleScorer.js
+++ assignment2ScrabbleScorer.js
@@ -206,7 +206,7 @@ To access a particular element within a letter array, add a second set of bracke
 function transform(object) {
   let newKey = {}
   for (item in object) {
-    newKey[item] = (object['key']);
+    newKey[item] = Number((object['key']));
     //newKey[item] = (item[object]);
     //test
     //console.log(newKey);
