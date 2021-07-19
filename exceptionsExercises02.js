d48fe197ce43ca6a5e38fb16830a0095514c1e49
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-07-19 17:43:39 -0400

    Update exceptionsExercises02.js
    at the second step of the exercise. Next, I will try the 'try' and 'catch' functions.

--- exceptionsExercises02.js
+++ exceptionsExercises02.js
@@ -21,4 +21,31 @@ let studentLabs = [
   }
 ];
 
-gradeLabs(studentLabs);
+let studentLabs2 = [
+   {
+      student: 'Blake',
+      myCode: function (num) {
+         return Math.pow(num, num);
+      }
+   },
+   {
+      student: 'Jessica',
+      runLab: function (num) {
+         return Math.pow(num, num);
+      }
+   },
+   {
+      student: 'Mya',
+      runLab: function (num) {
+         return num * num;
+      }
+   }
+];
+
+
+//gradeLabs(studentLabs);
+
+// per exercise
+gradeLabs(studentLabs2);
+
+
