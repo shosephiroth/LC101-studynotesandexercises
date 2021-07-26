d6115ac3d4cae0d5adaf8f9f7a0182751388b3cd
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-07-26 09:45:40 -0400

    Update exercisesDataAndVariables4.9.js
    added variables and used 'console.log()' to print variable types using 'typeof()'

--- exerciseReDos/exercisesDataAndVariables4.9.js
+++ exerciseReDos/exercisesDataAndVariables4.9.js
@@ -1,8 +1,18 @@
 // 1. Declare and assign the variables here:
 let shuttleName = '';
+let shuttleSpeed = 17500;
+let distanceToMars = 225000000;
+let distanceToMoon = 384400;
+let milesPerKilometer = 0.621;
 
 
 // 2. Use console.log to print the 'typeof' each variable. Print one item per line.
+console.log(typeof(shuttleName));
+console.log(typeof(shuttleSpeed));
+console.log(typeof(distanceToMars));
+console.log(typeof(distanceToMoon));
+console.log(typeof(milesPerKilometer));
+//console.log(typeof());
 
 
 
